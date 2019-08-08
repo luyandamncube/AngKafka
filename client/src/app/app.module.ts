59e5f9c5d2e3909defb20cccb94972df8383bb10
Luyanda Mncube (Britehouse) <luyanda.mncube@britehouse.co.za>
2019-08-08 13:48:44 +0200

    Corrected connection string

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,22 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { AppComponent } from './app.component';
-import { ChartModule } from 'angular2-highcharts/index';
-declare var require: any;
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { AppComponent } from "./app.component";
+import { ChartModule } from "angular2-highcharts/index";
+import { HighchartsStatic } from "angular2-highcharts/dist/HighchartsService";
+
+export declare let require: any;
+
+export function highchartsFactory() {
+  const hc = require("highcharts/highstock");
+  const dd = require("highcharts/modules/exporting");
+  dd(hc);
+
+  return hc;
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-  	ChartModule.forRoot(require('highcharts/highstock')),
+    // ChartModule.forRoot(require('highcharts/highstock')),
+    ChartModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HighchartsStatic,
+      useFactory: highchartsFactory
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- service/server.js
+++ service/server.js
@@ -4,6 +4,8 @@ let http = require('http').Server(app);
 let io = require('socket.io')(http);
 var Kafka = require('no-kafka');
 
+// var producer = new Kafka.Producer();
+
 
 io.on('connection', (socket) => {
   console.log('USER CONNECTED');
@@ -17,7 +19,7 @@ io.on('connection', (socket) => {
 http.listen(8091, () => {
   console.log('started on port 8091');
   var consumer = new Kafka.SimpleConsumer({
-        connectionString: 'MWKS363613:9092',
+        connectionString: 'localhost:9092',
         clientId: 'no-kafka-client'
     }); 
  
@@ -36,10 +38,26 @@ http.listen(8091, () => {
 		});
 	};
  
+	// return producer.init().then(function(){
+	// 	return producer.send({
+	// 		topic: 'test',
+	// 		partition: 0,
+	// 		message: {
+	// 			value: 'Hello!'
+	// 		}
+	// 	});
+	//   })
+	//   .then(function (result) {
+	// 	/*
+	// 	[ { topic: 'kafka-test-topic', partition: 0, offset: 353 } ]
+	// 	*/
+	//   });
 	return consumer.init().then(function () {
 		// Subscribe partitons 0 and 1 in a topic: 
 		var v1= consumer.subscribe('test', [0, 1], dataHandler);
+		console.log("Subscried to topic: 'test'");
 		var v2= consumer.subscribe('Sample', [0, 1], dataHandler);
+		console.log("Subscried to topic: 'Sample'");
 		var arr=[];
 		arr.push([v1,v2]);
 		console.log("val:"+arr);
